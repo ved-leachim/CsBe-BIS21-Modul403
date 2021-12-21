be3c863ae19e350619092510e1f8d0c9dcdb7aea
David Panchalingam <david.panchalingam@ict.csbe.ch>
2021-12-21 20:24:38 +0100

    Start

---
+++ src/Aufgaben/Arbeitsblatt1.java
@@ -0,0 +1,9 @@
+package Aufgaben;
+
+public class Arbeitsblatt1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This is not my first Java Programm");
+        System.out.println("That is Awsome!!");
+    }
+}
