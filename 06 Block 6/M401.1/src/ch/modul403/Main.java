87f98eac2c953bf2a08b0f144703a48c637ff7a6
Jerome <jerome.poffet@gmx.ch>
2021-12-21 21:16:47 +0100

    Block6

---
+++ 06 Block 6/M401.1/src/ch/modul403/Main.java
@@ -0,0 +1,15 @@
+package ch.modul403;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CalendarEvent calendarEventObjekt = new CalendarEvent();
+        calendarEventObjekt.eingabe();
+
+
+    }
+}
