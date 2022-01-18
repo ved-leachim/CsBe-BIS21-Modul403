9b6ba7b4a6e8567e91aaefec35a50a8b9c38da49
Michael Schmitz <michael.schmitz@swissuccess.ch>
2022-01-18 19:29:34 +0100

    Updating Block 8

--- 07 Block 7/09 SumOfEvenNumbers Debugging/src/ch/csbe/modul403/SumOfEvenNumbers.java
+++ 07 Block 7/09 SumOfEvenNumbers Debugging/src/ch/csbe/modul403/SumOfEvenNumbers.java
@@ -7,24 +7,24 @@ public class SumOfEvenNumbers {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int userInput;
+        long userInput;
 
         System.out.println("=========================================");
         System.out.println("Willkommen beim Delta Geradenummernzähler");
         System.out.println("=========================================");
-        System.out.println("Bitte geben Sie eine Zahl ein, die grösser als 2 ist:");
+        System.out.println("Bitte geben Sie eine Zahl ein, die grösser als 1 ist:");
 
         userInput = Integer.parseInt(scanner.nextLine());
-        if (userInput <= 2) {
-            System.out.println("Hey kannst du nicht lesen, grösser als 2!");
+        if (userInput < 2) {
+            System.out.println("Hey kannst du nicht lesen, grösser als 1!");
             System.out.println("Programm wird beendet!");
             System.exit(0);
         }
 
-        int currentNumber = 2;
-        int sumOfEvenNumbers = 0;
+        long currentNumber = 2;
+        long sumOfEvenNumbers = 0;
 
-        while (currentNumber < userInput) {
+        while (currentNumber <= userInput) {
             sumOfEvenNumbers += currentNumber;
             currentNumber += 2;
         }
